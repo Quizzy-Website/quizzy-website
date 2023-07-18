756ffbbd96d959fd552d13a03af7116755904a46
“shrutipanjwani” <hello@shrutipanjwani.com>
2023-07-18 18:23:16 +0530

    initial nextjs project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-yellow": "#F7D047",
+        "secondary-yellow": "#F3E24D",
+        "primary-grey": "#2C2D2D",
+        black: "#040404",
+        white: "#ffffff",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        quicksand: ["Quicksand Regular", "sans-serif"],
+        "quicksand-bold": ["Quicksand Bold", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
