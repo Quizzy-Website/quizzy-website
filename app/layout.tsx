984f7170342e7c91d6379259db14cf0dccf58c74
“shrutipanjwani” <hello@shrutipanjwani.com>
2023-07-25 21:21:44 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import Head from "next/head";
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Quizzy",
   description: "Powerhouse for Remote Team Collaboration",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +21,22 @@ export default function RootLayout({
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="icon" type="image/png" href="/favicon.png" />
+        {/* Google tag (gtag.js)  */}
+        <>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_G_ANALYTICS_ID}`}
+          />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${process.env.NEXT_PUBLIC_G_ANALYTICS_ID}');
+            `}
+          </Script>
+        </>
       </Head>
       <body>{children}</body>
     </html>
