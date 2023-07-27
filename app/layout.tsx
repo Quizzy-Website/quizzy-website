972c87a240ca39fda45c1850404b4f00c9276535
“shrutipanjwani” <hello@shrutipanjwani.com>
2023-07-27 11:54:30 +0530

    fixed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import Head from "next/head";
 import "./globals.css";
 import type { Metadata } from "next";
@@ -18,27 +19,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/favicon.png" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-
-        {/* Google tag (gtag.js)  */}
-        <>
-          <Script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_G_ANALYTICS_ID}`}
-          />
-          <Script id="google-analytics">
-            {`
+      {/* Google tag (gtag.js)  */}
+      <>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_G_ANALYTICS_ID}`}
+        />
+        <Script id="google-analytics">
+          {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
             
               gtag('config', '${process.env.NEXT_PUBLIC_G_ANALYTICS_ID}');
             `}
-          </Script>
-        </>
+        </Script>
+      </>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
+
       <body>{children}</body>
     </html>
   );
